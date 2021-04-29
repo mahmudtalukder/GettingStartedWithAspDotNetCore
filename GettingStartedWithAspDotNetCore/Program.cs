c85fe24d5d58570b3db3f3f09a73e1d3ed2a7fd2
mahmudtalukder <mtalukder@luc.edu>
2021-04-29 15:15:27 -0500

    Adding project template

---
+++ GettingStartedWithAspDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GettingStartedWithAspDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
