096272bab6c0611ce9041f8245c13c9eb5a16a3d
mahmudtalukder <mtalukder@luc.edu>
2021-04-29 15:19:12 -0500

    Adding a class file

---
+++ GettingStartedWithAspDotNetCore/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettingStartedWithAspDotNetCore
+{
+
+    //this is a blank class file
+    class TestClass
+    {
+    }
+}
